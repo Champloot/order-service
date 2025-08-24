2f0edf8e2fc6e16a5e7687463716b17da5d154ae
Artem Verchenko <artem.avert@gmail.com>
2025-08-24 21:05:41 +0300

    Change order.go

--- internal/models/order.go
+++ internal/models/order.go
@@ -18,3 +18,40 @@ type Order struct {
 	DateCreated			time.Time	`json:"date_created"`
 	OofShard			string		`json:"oof_shard"`
 }
+
+type Delivery struct {
+	Name				string		`json:"name"`
+	Phone				string		`json:"phone"`
+	Zip					string		`json:"zip"`
+	City				string		`json:"city"`
+	Address				string		`json:"address"`
+	Region				string		`json:"region"`
+	Email				string		`json:"email"`
+}
+
+type Payment struct {
+	Transaction			string		`json:"transaction"`
+	RequestID			string		`json:"request_id"`
+	Currency			string		`json:"currency"`
+	Provider			string		`json:"provider"`
+	Amount				int			`json:"amount"`
+	PaymentDT			int64		`json:"payment_dt"`
+	Bank				string		`json:"bank"`
+	Delivery			int			`json:"delivery_cost"`
+	GoodesTotal			int			`json:"goods_total"`
+	CustomFee			int			`json:"custom_fee"`
+}
+
+type  Item struct {
+	ChrtID				int			`json:"chrt_id"`
+	TrackNumber			string		`json:"track_number"`
+	Price				int			`json:"price"`
+	RID					string		`json:"rid"`
+	Name				string		`json:"name"`
+	Sale				int			`json:"sale"`
+	Size				string		`json:"size"`
+	TotalPrice			int			`json:"total_price"`
+	NmID				int			`json:"nm_id"`
+	Brand				string		`json:"brand"`
+	Status				int			`json:"status"`
+}
