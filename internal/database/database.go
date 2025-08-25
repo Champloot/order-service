110f0f75c1c5a5820421e5f4d054aea033294ad2
Artem Verchenko <artem.avert@gmail.com>
2025-08-25 20:21:23 +0300

    Change database

--- internal/database/database.go
+++ internal/database/database.go
@@ -2,10 +2,12 @@ package database
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"time"
 
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
@@ -35,4 +37,40 @@ func NewPostgresRepository(ctx context.Context, connString string) (*PostgresRep
 
 	log.Println("Successfully connected to PostgreSQL")
 	return repo, nil
+}
+
+func (r *PostgresRepository) createTables(ctx context.Context) error {
+	query := `
+		CREATE TABLE IF NOT EXISTS orders (
+			order_uid VARCHAR(255) PRIMARY KEY,
+			track_number VARCHAR(255),
+			entry VARCHAR(255),
+			delivery JSONB NOT NULL,
+			payment JSONB NOT NULL,
+			items JSONB NOT NULL,
+			locale VARCHAR(255),
+			internal_signature VARCHAR(255),
+			customer_id VARCHAR(255),
+			delivery_service VARCHAR(255),
+			shardkey VARCHAR(255),
+			sm_id INTEGER,
+			date_created TIMESTAMP WITH TIME ZONE,
+			oof_shard VARCHAR(255)
+		);
+
+		CREATE INDEX IF NOT EXISTS idx_orders_order_uid ON orders(order_uid);
+		CREATE INDEX IF NOT EXISTS idx_orders_date_create ON orders(date_created);
+	`
+
+	_, err := r.pool.Exec(ctx, query)
+	if err != nil {
+		return fmt.Errorf("Failed to create tables: %w", err)
+	}
+
+	log.Println("Tables created or already exist")
+	return nil
+}
+
+func (r *PostgresRepository) Close() {
+	r.pool.Close()
 }
