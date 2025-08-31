2b48c3d2976e38929885d1fb406fef242412c5fd
Artem Verchenko <artem.avert@gmail.com>
2025-08-31 14:56:10 +0300

    End with Kafka (maybe)

--- internal/kafka/consumer.go
+++ internal/kafka/consumer.go
@@ -43,3 +43,50 @@ func NewConsumer(config *KafkaConfig, db *database.PostgresDB) *Consumer {
 	}
 }
 
+func (c *Consumer) Start(ctx context.Context) {
+	log.Println("Starting Kafka consumer...")
+
+	for {
+		select {
+			case <-ctx.Done():
+				return
+			default:
+				msg, err := c.reader.ReadMessage(ctx)
+				if err != nil {
+					log.Printf("Error reading message: %v", err)
+					continue
+				}
+
+				log.Printf("Recived message: %s", string(msg.Value))
+
+				if err := c.processMessage(ctx, msg.Value); err != nil {
+					log.Printf("Error processing message: %v", err)
+				}
+		}
+	}
+}
+
+
+func (c *Consumer) processMessage(ctx context.Context, data []byte) error {
+	var order models.Order
+	if err := json.Unmarshal(data, &order); err != nil {
+		return fmt.Errorf("Failed to unmarshal order: %v", err)
+	}
+
+	// data validation
+	if order.OrderUID == "" {
+		return fmt.Errorf("Order UID is required")
+	}
+
+	// save
+	if err := c.db.SaveOrder(ctx, &order); err != nil {
+		return fmt.Errorf("Failed to save order to database: %v", err)		
+	}
+
+	log.Printf("Successfully processed order %s", order.OrderUID)
+	return nil
+}
+
+func (c *Consumer) Close() error {
+	return c.reader.Close()
+}
